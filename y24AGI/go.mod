8048e61d8c127bf28b47b1fcb078a1f790f93576
Chippers255 <tn90ca@gmail.com>
2024-12-01 00:54:30 -0500

    day 1 is complete

---
+++ y24AGI/go.mod
@@ -0,0 +1,3 @@
+module y24AGI
+
+go 1.21.5
--- year2024/internal/aoc/day01/day01.go
+++ year2024/internal/aoc/day01/day01.go
@@ -3,26 +3,83 @@ package day01
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-func Part1(input string) (int, error) {
-	file, err := os.Open(input)
+func readInputFile(filePath string) ([]int, []int, error) {
+	content, err := os.ReadFile(filePath)
 	if err != nil {
-		return 0, fmt.Errorf("failed to read input file: %w", err)
+		return nil, nil, fmt.Errorf("error reading file: %v", err)
 	}
-	defer file.Close()
 
-	return 42, nil
+	lines := strings.Split(strings.TrimSpace(string(content)), "\n")
+
+	l1 := make([]int, 0, len(lines))
+	l2 := make([]int, 0, len(lines))
+
+	for _, line := range lines {
+		numbers := strings.Fields(line)
+		num1, _ := strconv.Atoi(numbers[0])
+		num2, _ := strconv.Atoi(numbers[1])
+		l1 = append(l1, num1)
+		l2 = append(l2, num2)
+	}
+
+	return l1, l2, nil
+}
+
+func sumList(list []int) int {
+	sum := 0
+	for _, v := range list {
+		sum += v
+	}
+	return sum
+}
+
+func absDiff(a, b int) int {
+	diff := a - b
+	if diff < 0 {
+		diff = -diff
+	}
+	return diff
+}
+
+func Part1(input string) (int, error) {
+	l1, l2, err := readInputFile(input)
+	if err != nil {
+		return 0, err
+	}
+
+	sort.Ints(l1)
+	sort.Ints(l2)
+
+	diff := make([]int, len(l1))
+	for i := range l1 {
+		diff[i] = absDiff(l1[i], l2[i])
+	}
+	sum := sumList(diff)
+
+	return sum, nil
 }
 
 func Part2(input string) (int, error) {
-	file, err := os.Open(input)
+	l1, l2, err := readInputFile(input)
 	if err != nil {
-		return 0, fmt.Errorf("failed to read input file: %w", err)
+		return 0, err
 	}
-	defer file.Close()
 
-	return 84, nil
+	sum := 0
+	for _, n1 := range l1 {
+		for _, n2 := range l2 {
+			if n1 == n2 {
+				sum += n1
+			}
+		}
+	}
+
+	return sum, nil
 }
 
 func Run(input string) error {
